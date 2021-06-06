3f6eb0c189ef059a744085226f230bcaa42ea0f1
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-06 17:56:47 -0300

    Transformação de Enum para Interface nas permissões.

--- src/main/java/team/martin/hub/utils/PermissionsUtils.java
+++ src/main/java/team/martin/hub/utils/PermissionsUtils.java
@@ -1,17 +1,6 @@
 package team.martin.hub.utils;
 
-public enum PermissionsUtils {
-
-    STAFF("hub.staff");
-
-    private String permission;
-
-    public String getPermission() {
-        return this.permission;
-    }
-
-    PermissionsUtils(String permission) {
-        this.permission = permission;
-    }
-
+public interface PermissionsUtils {
+    String STAFF = "hub.staff";
+    String USER = "hub.user";
 }
