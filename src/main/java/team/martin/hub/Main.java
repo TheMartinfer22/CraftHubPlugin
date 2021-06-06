8b74ef1741824341af6c7196ac883fbf1ce110f7
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-06 17:55:54 -0300

    Criação do comando ban.

--- src/main/java/team/martin/hub/Main.java
+++ src/main/java/team/martin/hub/Main.java
@@ -2,7 +2,6 @@ package team.martin.hub;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import team.martin.hub.chat.ChatTags;
-import team.martin.hub.chat.commands.AdminCommands;
 import team.martin.hub.chat.commands.admin.BanPlayer;
 
 
@@ -10,15 +9,15 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         getServer().getPluginManager().registerEvents(new ChatTags(), this);
         getCommand("ban").setExecutor(new BanPlayer());
-
-
-
     }
 
     @Override
     public void onDisable() {
+    }
+
+    public static Main getInstance(){
+        return getPlugin(Main.class);
     }
 }
--- src/main/java/team/martin/hub/chat/commands/admin/BanPlayer.java
+++ src/main/java/team/martin/hub/chat/commands/admin/BanPlayer.java
@@ -3,30 +3,49 @@ package team.martin.hub.chat.commands.admin;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import team.martin.hub.Main;
 import team.martin.hub.utils.MessagesUtils;
 import team.martin.hub.utils.PermissionsUtils;
 
 import java.sql.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.bukkit.Bukkit.getBanList;
 
 public class BanPlayer implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player p = Bukkit.getServer().getPlayer(args[0]);
-        if (sender.hasPermission(PermissionsUtils.STAFF.getPermission())){
-            if (!args[0].isEmpty()){
-                Bukkit.getBanList(BanList.Type.NAME).addBan(p.getDisplayName(), MessagesUtils.banPlayerReason, new Date(300000), sender.getName());
-                sender.sendMessage(ChatColor.GREEN + "Você baniu o jogador " + p.getPlayer().getDisplayName() + " com sucesso!");
-            } else {
+
+        if (sender.hasPermission(PermissionsUtils.STAFF)) {
+
+            if (args.length < 1) {
                 sender.sendMessage(MessagesUtils.banPlayerIncomplete);
+                return true;
             }
-            return true;
+
+            OfflinePlayer p = Bukkit.getServer().getOfflinePlayer(args[0]);
+
+            if (p.getPlayer().isBanned()){
+                sender.sendMessage(MessagesUtils.hasBanned);
+                return true;
+            }
+
+            Optional.of(args[0]).ifPresent(action-> {
+                getBanList(BanList.Type.NAME).addBan(p.getName(), MessagesUtils.banPlayerReason, null, sender.getName());
+                sender.sendMessage(MessagesUtils.prefix + ChatColor.GREEN + "Você baniu o jogador " + p.getName() + " com sucesso!");
+                p.getPlayer().kickPlayer(MessagesUtils.banPlayerReason);
+            });
+            return false;
+
         } else {
             sender.sendMessage(MessagesUtils.noPermission);
         }
-        return false;
+        return true;
     }
 }
