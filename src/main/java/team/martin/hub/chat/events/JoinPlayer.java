05502c422173324883bffcf34f9ad114d566252b
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-07 01:49:44 -0300

    Correção da mensagem ao entrar.

--- src/main/java/team/martin/hub/chat/events/JoinPlayer.java
+++ src/main/java/team/martin/hub/chat/events/JoinPlayer.java
@@ -1,15 +1,15 @@
 package team.martin.hub.chat.events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import team.martin.hub.utils.MessagesUtils;
 
 public class JoinPlayer implements Listener {
-
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         event.getPlayer().sendMessage(MessagesUtils.onJoin);
-        System.out.println(event.getJoinMessage());
+        event.setJoinMessage(ChatColor.GREEN + "[+] " + ChatColor.RESET + event.getPlayer().getDisplayName());
     }
 }
