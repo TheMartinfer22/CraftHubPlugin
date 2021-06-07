8458ea7df0401a81e20c2d1420bc2d2e30158564
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-07 01:50:03 -0300

    Adicionado o evento de saída de jogadores.

--- src/main/java/team/martin/hub/management/Events.java
+++ src/main/java/team/martin/hub/management/Events.java
@@ -4,14 +4,14 @@ import team.martin.hub.Main;
 import team.martin.hub.chat.events.ChatBadMessage;
 import team.martin.hub.chat.events.ChatTags;
 import team.martin.hub.chat.events.JoinPlayer;
+import team.martin.hub.chat.events.LeftPlayer;
 
 public class Events {
 
     public Events(){
         Main.getInstance().getServer().getPluginManager().registerEvents(new ChatTags(), Main.getInstance());
         Main.getInstance().getServer().getPluginManager().registerEvents(new JoinPlayer(), Main.getInstance());
+        Main.getInstance().getServer().getPluginManager().registerEvents(new LeftPlayer(), Main.getInstance());
         Main.getInstance().getServer().getPluginManager().registerEvents(new ChatBadMessage(), Main.getInstance());
-
     }
-
 }
