0407b5be541be8486bf30de3a7b41d2f6afebbfd
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-06-09 00:21:43 -0300

    Função de lag em pausa.

---
+++ src/main/java/team/martin/hub/chat/events/ClearItens.java
@@ -0,0 +1,35 @@
+// TODO Feature Future
+//package team.martin.hub.chat.events;
+//
+//import org.bukkit.Bukkit;
+//import team.martin.hub.Main;
+//
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class ClearItens {
+//    public void startTimer(){
+//
+//    }
+//    public void mensagem1(){
+//
+//    }
+//    public void mensagem2(){
+//
+//    }
+//    public void mensagem3(){
+//
+//    }
+//    public void mensagem4(){
+//
+//    }
+//    public void mensagem5(){
+//
+//    }
+//    public void mensagem6(){
+//
+//    }
+//    public void mensagem7(){
+//
+//    }
+//}
